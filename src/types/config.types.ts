5d10ed7e9bd1b8e10576b29e93ddc41c80518fd1
Minh Doan <minhdpc@gmail.com>
2026-03-16 17:46:37 +0700

    Refactor database configuration: rename user to username and add DbConfig class for database settings

--- src/types/config.types.ts
+++ src/types/config.types.ts
@@ -5,3 +5,14 @@ export type IConfigArray = IConfigValue[];
 export interface IConfigObject {
   [key: string]: IConfigValue;
 }
+
+export interface IDbConfig {
+  host: string;
+  port: number;
+  username: string;
+  password: string;
+  name: string;
+  pool_min: number;
+  pool_max: number;
+  debug: boolean;
+}
