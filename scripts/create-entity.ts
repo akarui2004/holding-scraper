c2146670b64875955b539445ac632cc05aa2bcd7
Minh Doan <minhdpc@gmail.com>
2026-04-02 23:21:35 +0700

    Refactor entity creation script and template to support dynamic class names

--- scripts/create-entity.ts
+++ scripts/create-entity.ts
@@ -6,24 +6,24 @@ import { plural } from 'pluralize';
 import { sprintf } from 'sprintf-js';
 
 const createEntityFile = (entityName: string) => {
-  const lowerCaseEntityName = entityName.toLowerCase();
-  const fileName = `${lowerCaseEntityName}.entity.ts`;
-  const tableName = plural(lowerCaseEntityName);
-
   const templatePath = path.join(__dirname, 'template', 'create-entity.template');
   if (!fs.existsSync(templatePath)) {
     console.error(pc.red(`❌ Template file not found at ${templatePath}`));
     return;
   }
-  const content = fs.readFileSync(templatePath, 'utf-8');
-  const entityContent = sprintf(content, tableName);
 
+  const lowerCaseEntityName = entityName.toLowerCase();
+  const fileName = `${lowerCaseEntityName}.entity.ts`;
   const entityFilePath = path.join(__dirname, '..', 'src', 'entities', fileName);
   if (fs.existsSync(entityFilePath)) {
     console.error(pc.red(`❌ Entity already exists: src/entities/${fileName}`));
     return;
   }
 
+  const content = fs.readFileSync(templatePath, 'utf-8');
+  const tableName = plural(lowerCaseEntityName);
+  const entityClassName = `${lowerCaseEntityName.charAt(0).toUpperCase() + lowerCaseEntityName.slice(1)}Entity`;
+  const entityContent = sprintf(content, tableName, entityClassName);
   fs.writeFileSync(entityFilePath, entityContent);
   console.log(pc.green(`✅ Created entity: src/entities/${fileName}`));
 };
